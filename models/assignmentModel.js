42c3891c77eebff87f1c26c0a80a1b80f14e360b
yohannes444 <yohannesmulat444@gmail.com>
2023-04-28 01:56:05 +0300

    forms

---
+++ models/assignmentModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const assignmentSchema = new mongoose.Schema({
+  
+  
+    subject: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    quation: {
+        type: String,
+    },
+    quationFile:{
+        type:String
+    },
+    classRoom: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Classroom',
+    },
+    teacher: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'user',
+}
+});
+
+const Assignment = mongoose.model('material', assignmentSchema);
+
+module.exports = Assignment;
